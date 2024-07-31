0988fb515a40d0ee4925aceafcd70056cc87727a
kavyansh18 <kavyanshkumar18@gmail.com>
2024-07-31 23:33:24 +0530

    feat:donatebtn

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage.jsx';
 import HomePage from './Pages/HomePage.jsx'
 import Explore from './Pages/Explore.jsx';
+import Donate from './Pages/Donate.jsx';
 
 const OKTO_CLIENT_API_KEY = "3469fac8-410e-4fab-8f16-a0345461045c";
 
@@ -17,7 +18,7 @@ function App() {
           <Route path="/home" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/explore" element={<Explore />} />
-          <Route path="/donate" element={<HomePage />} />
+          <Route path="/donate" element={<Donate />} />
         </Routes>
       </OktoProvider>
     </Router>
