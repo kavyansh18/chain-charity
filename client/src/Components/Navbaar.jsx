e023baa096828792d945c0ea3d75d808b188f4bd
kavyansh18 <kavyanshkumar18@gmail.com>
2024-08-01 23:18:57 +0530

    feat:logo

--- client/src/Components/Navbaar.jsx
+++ client/src/Components/Navbaar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
+import logo from '../assets/logo.png'
 
 const Navbaar = () => {
   return (
-    <div className=' w-screen' >
+    <div className=' w-screen h-16' >
       <header >
-        <div className="logo">NGO Donations</div>
-        <nav>
+        <div className="logo mt-3"><img className='w-32 rounded-3xl' src={logo} alt="" /></div>
+        <nav className='mt-2'>
         <NavLink to="/explore">
             Home
           </NavLink>
